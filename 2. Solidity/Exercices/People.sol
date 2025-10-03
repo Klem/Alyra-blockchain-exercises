1fc9543a999e35024c218dabbaf48093a1bf2b80
Klem <klem86@protonmail.com>
2025-10-03 14:36:16 +0200

    Array manipulation

--- 2. Solidity/Exercices/People.sol
+++ 2. Solidity/Exercices/People.sol
@@ -8,9 +8,28 @@ contract People {
     }
 
     Person public moi = Person("Klem", 43);
+    Person[] public persons;
 
+    /**
+    * Modify the existing "Person" member
+    */
     function  modifyPerson(string memory _name, uint8 _age) public {
         moi.name = _name;
         moi.age = _age;
+    }
+
+    /**
+    * Add a new Person to the person array
+    */
+    function addPerson(string memory _name, uint8 _age) public {
+        persons.push(Person(_name, _age));
+    }
+    
+    
+    /**
+    * Remove the last added Person fron the person array
+    */
+    function removeLastPerson() public {
+        persons.pop();
     }
 }
