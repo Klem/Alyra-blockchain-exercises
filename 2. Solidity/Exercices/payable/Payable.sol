e569c560508235b7bfe5b6964e803ac14ccc34fd
Klem <klem86@protonmail.com>
2025-10-08 18:18:40 +0200

    added send / transfer differences

--- 2. Solidity/Exercices/payable/Payable.sol
+++ 2. Solidity/Exercices/payable/Payable.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.8.28;
 // https://rareskills.io/learn-solidity/payable-functions
 contract Payable {
 
-    function sendEther() payable public {
+    function sendEtherToContract() payable public {
 
     }
+
+    function sendViaTransfert(address payable _to) payable public {
+            _to.transfer(msg.value);
+    }
+
+        function sendViaSend(address payable _to) payable public {
+           bool ok =  _to.send(msg.value);
+           require(ok, "Transfert failed");
+    }
+
 
     function amIRich() view public returns (uint) {
         return address(this).balance;
